a2ecf12bb58f06fd9ec1ca1e561c6ba140924b8b
Murad <muradahamed6262@gmail.com>
2022-02-01 22:01:11 +0600

    modal practise

---
+++ modal/modal.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('click',function(){console.log('click')
+ document.querySelector('.modal').classList.toggle('hidden');
+    document.querySelector('.overlay').classList.toggle('hidden');
+
+});
